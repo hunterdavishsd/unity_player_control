1cd171c55110e1030b1d88e7b0b24ed8ccbbe5fd
Davis_Hunter <davih604@hsd.k12.or.us>
2025-03-06 10:33:34 -0800

    adding speed, turns, stopping. rotates

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
     {
         public float speed = 5.0f;
-
-
+        public float turnSpeed;
+        public float hotizontalInput;
+        public float forwardInput;
       // Start is called once before the first execution of Update after the MonoBehaviour is created
       void Start()
      {
@@ -16,8 +17,12 @@ public class PlayerController : MonoBehaviour
      // Update is called once per frame
      void Update()
         {
+        hotizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+       
        //move the tank
     
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); 
-      }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+        transform.Rotate(Vector3.up, turnSpeed * hotizontalInput * Time.deltaTime);
+        }
     }
