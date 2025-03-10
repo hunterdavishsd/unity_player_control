88ff8dca0329ee020307b2949b863532ea503e50
Davis_Hunter <davih604@hsd.k12.or.us>
2025-03-10 11:38:52 -0700

    plane challange

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float forwardInput;
+    public float turnSpeed;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.forward * verticalInput * Time.deltaTime * turnSpeed);
+    }
+}
---
+++ Assets/Challenge 1/Scripts/propeller.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class propeller : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    public float speed;
+    public float Rotate;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+       
+    }
+}
+
+
